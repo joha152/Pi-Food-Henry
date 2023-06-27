bc58760e845aae840b93515651c1d0f9f7012590
joha152 <johanna.servodio@hotmail.com.ar>
2023-06-27 09:46:13 -0300

    arreglando el index

--- api/index.js
+++ api/index.js
@@ -19,13 +19,15 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-require("dotenv").config();
 
-const port = process.env.PORT 
+
+
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
-  server.listen(port, () => {
-    console.log(`Server in port ${port}`); // eslint-disable-line no-console
+conn
+.sync({ alter: true })
+.then(() => {
+  server.listen(3001, () => {
+    console.log(`Server in port 3001`); // eslint-disable-line no-console
   });
 });
